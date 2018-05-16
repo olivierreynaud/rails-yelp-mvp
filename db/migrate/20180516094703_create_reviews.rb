1fb537fe72a9337929bb3115da3004f2fa435204
Olivier Reynaud <olivreynaud@gmail.com>
2018-05-16 12:15:41 +0200

    validations done

--- db/migrate/20180516094703_create_reviews.rb
+++ db/migrate/20180516094703_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.string :description
-      t.string :rating
+      t.string :content
+      t.integer :rating
       t.references :restaurant, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,15 @@ ActiveRecord::Schema.define(version: 2018_05_16_094703) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "description"
-    t.string "rating"
+    t.string "content"
+    t.integer "rating"
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
